e715aea371a68b7eb3eb26530c4a5620fd983770
BringerOfPlagues <si.wuerfele2@gmail.com>
2025-12-04 18:08:51 +0100

    Grundgerüst für eine Toolbar implementiert, mit der man einfache Funktionen über Buttons ausführen kann. Tooltips für diese Buttons implementiert. Code besser angeordnet, damit es übersichtlicher ist. Die Bilddateien für die Buttons liegen jetzt auch schon mal im Projekt.

--- src/Grafikprogramm.java
+++ src/Grafikprogramm.java
@@ -1,4 +1,5 @@
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
@@ -19,6 +20,47 @@ public class Grafikprogramm extends JFrame {
         JMenu menuDatei = new JMenu("Datei");
         menuBar.add(menuDatei);
 
+        //Menü-Funktion "Neu", um neue Datei zu erstellen
+        JMenuItem neuItem = new JMenuItem("Neu");
+        menuDatei.add(neuItem);
+
+        neuItem.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+                //FUNKTION MUSS NOCH IMPLEMENTIERT WERDEN!!!!!
+
+            }
+        });
+
+        //Menü-Funktion "Öffnen", um eine Datei zu öffnen
+        JMenuItem oeffnenItem = new JMenuItem("Öffnen");
+        menuDatei.add(oeffnenItem);
+
+        oeffnenItem.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+                //FUNKTION MUSS NOCH IMPLEMENTIERT WERDEN!!!!!
+
+            }
+        });
+
+        //Menü-Funktion "Speichern", um eine Datei zu speichern
+        JMenuItem speichernItem = new JMenuItem("Speichern");
+        menuDatei.add(speichernItem);
+
+        speichernItem.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+                //FUNKTION MUSS NOCH IMPLEMENTIERT WERDEN!!!!!
+
+            }
+        });
+
+        menuDatei.addSeparator(); //Trennstrich, sieht hübscher aus
+
         //Beenden-Funktion im Dateimenü erzeugen
         JMenuItem beenden = new JMenuItem("Beenden");
         menuDatei.add(beenden);
@@ -30,6 +72,51 @@ public class Grafikprogramm extends JFrame {
                 System.exit(0);
             }
         });
+
+        //Toolbar erzeugen mit einfachen Funktionen
+        JToolBar toolbar = new JToolBar();
+
+        JButton buttonNeu = new JButton("Neu");
+        buttonNeu.setToolTipText("Neue Datei"); //Einen Tipp anzeigen lassen, wenn man mit der Maus über das Feld geht
+        toolbar.add(buttonNeu);
+
+        buttonNeu.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+                //FUNKTION MUSS NOCH IMPLEMENTIERT WERDEN!!!!!
+
+            }
+        });
+
+        JButton buttonOeffnen = new JButton("Öffnen");
+        buttonOeffnen.setToolTipText("Datei öffnen");
+        toolbar.add(buttonOeffnen);
+
+        buttonOeffnen.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+                //FUNKTION MUSS NOCH IMPLEMENTIERT WERDEN!!!!!
+
+            }
+        });
+
+        JButton buttonSpeichern = new JButton("Speichern");
+        buttonSpeichern.setToolTipText("Datei speichern");
+        toolbar.add(buttonSpeichern);
+
+        buttonSpeichern.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+                //FUNKTION MUSS NOCH IMPLEMENTIERT WERDEN!!!!!
+
+            }
+        });
+
+        //Toolbar hinzufügen, Anordnung oben
+        add(toolbar, BorderLayout.NORTH);
 
         //Zeichenfeld in das Fenster hinzufügen
         zeichenfeld = new Zeichenfeld();
