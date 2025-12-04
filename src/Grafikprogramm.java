ef5c87a1b9eea9bd7b8cc01db3d351cd6891a466
BringerOfPlagues <si.wuerfele2@gmail.com>
2025-12-04 16:51:45 +0100

    Grundgerüst des Zeichenfelds erstellt und im Konstruktor des Grafikprogramms aufgerufen. Erstmal einfache Menüleiste eingefügt, die aktuell nur die Funktion "beenden" hat.

--- src/Grafikprogramm.java
+++ src/Grafikprogramm.java
@@ -1,14 +1,41 @@
-import javax.swing.JFrame;
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class Grafikprogramm extends JFrame {
-
+    // Hier wird das Fenster erstellt
+    public Zeichenfeld zeichenfeld; // zum später aufrufen
     public Grafikprogramm() {
-        // Hier wird das Fenster erstellt
         setTitle("Grafikprogramm");
         setSize(800, 600);
-        setLocationRelativeTo(null);
+        setLocationRelativeTo(null); //Fenster zentrieren
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        //Menüleiste erstellen und platzieren
+        JMenuBar menuBar = new JMenuBar();
+        setJMenuBar(menuBar);
+
+        //Dateimenü erzeugen
+        JMenu menuDatei = new JMenu("Datei");
+        menuBar.add(menuDatei);
+
+        //Beenden-Funktion im Dateimenü erzeugen
+        JMenuItem beenden = new JMenuItem("Beenden");
+        menuDatei.add(beenden);
+
+        //Schließen der Anwendung, wenn "beenden" betätigt wird
+        beenden.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.exit(0);
+            }
+        });
+
+        //Zeichenfeld in das Fenster hinzufügen
+        zeichenfeld = new Zeichenfeld();
+        add(zeichenfeld);
 
         setVisible(true);
     }
 }
+
---
+++ src/Zeichenfeld.java
@@ -0,0 +1,16 @@
+import javax.swing.*;
+import java.awt.Graphics;
+
+public class Zeichenfeld extends JPanel {
+    //Hier wird die Zeichenfläche erstellt
+    public Zeichenfeld () {
+
+    }
+
+    //Hier entsteht die Funktion zum Zeichnen später
+    //paintComponent wird von JComponents bereitgestellt
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+    }
+}
