c63ece7327fbf706d97fa905ff870256a2b1f0d0
Simon Würfele <si.wuerfele@yahoo.de>
2025-12-04 01:02:24 +0100

    Initial commit

---
+++ src/Grafikprogramm.java
@@ -0,0 +1,14 @@
+import javax.swing.JFrame;
+
+public class Grafikprogramm extends JFrame {
+
+    public Grafikprogramm() {
+        // Hier wird das Fenster erstellt
+        setTitle("Grafikprogramm");
+        setSize(800, 600);
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        setVisible(true);
+    }
+}
