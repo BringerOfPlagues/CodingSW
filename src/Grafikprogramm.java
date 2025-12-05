3e4d81914482ccb62e91ce65bed62cd078baaf22
BringerOfPlagues <si.wuerfele2@gmail.com>
2025-12-05 19:26:59 +0100

    Klasse Grafikprogramm aufgeräumt. Zuvor hatte ich für jeden Button/Menü-Funktion einen eigenen ActionListener verwendet. Jetzt gibt es einen ActionListener für alle Datei-Funktionen, in dem über ActionCommands die Funktionen ausgeführt werden. Als nächstes muss ich noch einen ActionListener für die Tools machen, haben aktuell noch jeder einen eigenen Listener.

--- src/Grafikprogramm.java
+++ src/Grafikprogramm.java
@@ -5,12 +5,47 @@ import java.awt.event.ActionListener;
 
 public class Grafikprogramm extends JFrame {
     // Hier wird das Fenster erstellt
-    public Zeichenfeld zeichenfeld; // zum später aufrufen
+    public Zeichenfeld zeichenfeld;// zum später aufrufen
+
+    final String actionNeu = "Neu";
+    final String actionOeffnen = "Öffnen";
+    final String actionSpeichern = "Speichern";
+    final String actionBeenden = "Beenden";
+    final String actionLinie = "Linie";
+    final String actionRechteck = "Rechteck";
+    final String actionEllipse = "Ellipse";
+
+
     public Grafikprogramm() {
         setTitle("Grafikprogramm");
         setSize(800, 600);
         setLocationRelativeTo(null); //Fenster zentrieren
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        ActionListener dateiListener = new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String action = e.getActionCommand();
+
+                switch (action) {
+                    case actionNeu:
+                        int sicher = JOptionPane.showConfirmDialog(Grafikprogramm.this,
+                                "Sind Sie sicher, dass Sie eine neue Datei anlegen wollen?\nNicht gespeicherte Änderungen gehen verloren!");
+
+                        //Betätigt man "Ja" wird sicher auf 0 gesetzt
+                        if (sicher == 0) zeichenfeld.leeren();
+                        break;
+
+                    case actionOeffnen:
+
+                    case actionSpeichern:
+
+                    case actionBeenden:
+                        System.exit(0);
+                        break;
+                }
+            }
+        };
 
         //Menüleiste erstellen und platzieren
         JMenuBar menuBar = new JMenuBar();
@@ -23,61 +58,28 @@ public class Grafikprogramm extends JFrame {
         //Menü-Funktion "Neu", um neue Datei zu erstellen
         JMenuItem neuItem = new JMenuItem("Neu");
         menuDatei.add(neuItem);
-
-        neuItem.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-
-                //TODO: Ein Action-Listener für Button und Items, da beide das selbe machen
-
-                //Ein neues Pop-up Fenster erscheint, in dem man bestätigen muss, dass die Fläche initialisiert wird
-                int sicher = JOptionPane.showConfirmDialog(Grafikprogramm.this,
-                "Sind Sie sicher, dass Sie eine neue Datei anlegen wollen?\nNicht gespeicherte Änderungen gehen verloren!");
-
-                //Betätigt man "Ja" wird sicher auf 0 gesetzt
-                if (sicher == 0) zeichenfeld.leeren();
-            }
-        });
+        neuItem.setActionCommand(actionNeu);
+        neuItem.addActionListener(dateiListener);
 
         //Menü-Funktion "Öffnen", um eine Datei zu öffnen
         JMenuItem oeffnenItem = new JMenuItem("Öffnen");
         menuDatei.add(oeffnenItem);
-
-        oeffnenItem.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-
-                //FUNKTION MUSS NOCH IMPLEMENTIERT WERDEN!!!!!
-
-            }
-        });
+        oeffnenItem.setActionCommand(actionOeffnen);
+        oeffnenItem.addActionListener(dateiListener);
 
         //Menü-Funktion "Speichern", um eine Datei zu speichern
         JMenuItem speichernItem = new JMenuItem("Speichern");
         menuDatei.add(speichernItem);
-
-        speichernItem.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-
-                //FUNKTION MUSS NOCH IMPLEMENTIERT WERDEN!!!!!
-
-            }
-        });
+        speichernItem.setActionCommand(actionSpeichern);
+        speichernItem.addActionListener(dateiListener);
 
         menuDatei.addSeparator(); //Trennstrich, sieht hübscher aus
 
         //Beenden-Funktion im Dateimenü erzeugen
         JMenuItem beenden = new JMenuItem("Beenden");
         menuDatei.add(beenden);
-
-        //Schließen der Anwendung, wenn "beenden" betätigt wird
-        beenden.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                System.exit(0);
-            }
-        });
+        beenden.setActionCommand(actionBeenden);
+        beenden.addActionListener(dateiListener);
 
         //Toolbar erzeugen mit einfachen Funktionen
         JToolBar toolbar = new JToolBar();
@@ -85,46 +87,20 @@ public class Grafikprogramm extends JFrame {
         JButton buttonNeu = new JButton("Neu");
         buttonNeu.setToolTipText("Neue Datei"); //Einen Tipp anzeigen lassen, wenn man mit der Maus über das Feld geht
         toolbar.add(buttonNeu);
-
-        buttonNeu.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-
-                //Ein neues Pop-up Fenster erscheint, in dem man bestätigen muss, dass die Fläche initialisiert wird
-                int sicher = JOptionPane.showConfirmDialog(Grafikprogramm.this,
-                "Sind Sie sicher, dass Sie eine neue Datei anlegen wollen?\nNicht gespeicherte Änderungen gehen verloren!");
-
-                //Betätigt man "Ja" wird sicher auf 0 gesetzt
-                if (sicher == 0) zeichenfeld.leeren();
-
-            }
-        });
+        buttonNeu.setActionCommand(actionNeu);
+        buttonNeu.addActionListener(dateiListener);
 
         JButton buttonOeffnen = new JButton("Öffnen");
         buttonOeffnen.setToolTipText("Datei öffnen");
         toolbar.add(buttonOeffnen);
-
-        buttonOeffnen.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-
-                //FUNKTION MUSS NOCH IMPLEMENTIERT WERDEN!!!!!
-
-            }
-        });
+        buttonOeffnen.setActionCommand(actionOeffnen);
+        buttonOeffnen.addActionListener(dateiListener);
 
         JButton buttonSpeichern = new JButton("Speichern");
         buttonSpeichern.setToolTipText("Datei speichern");
         toolbar.add(buttonSpeichern);
-
-        buttonSpeichern.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-
-                //FUNKTION MUSS NOCH IMPLEMENTIERT WERDEN!!!!!
-
-            }
-        });
+        buttonSpeichern.setActionCommand(actionSpeichern);
+        buttonSpeichern.addActionListener(dateiListener);
 
         JButton buttonLinie = new JButton("Linie");
         buttonLinie.setToolTipText("Werkzeug Linie");
